43b59f955ef917623efb49aa3f7b83e88204b7ec
impetus.opensource <impetus.opensource@d4f98007-0d38-2de3-70ff-3cc3a5ab524d>
2010-07-15 08:35:37 +0000

    adding a unit test for Validator test!

---
+++ src/test/java/com/impetus/kundera/junit/ValidatorTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010 Impetus Infotech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.impetus.kundera.junit;
+
+import com.impetus.kundera.entity.Author;
+import com.impetus.kundera.metadata.Validator;
+import com.impetus.kundera.metadata.ValidatorImpl;
+
+import junit.framework.TestCase;
+
+/**
+ * The Class ValidatorTest.
+ */
+public class ValidatorTest extends TestCase {
+
+    /**
+     * Test validate.
+     */
+    public void testValidate() {
+        Validator validator = new ValidatorImpl();
+        validator.validate(Author.class);
+    }
+
+}
