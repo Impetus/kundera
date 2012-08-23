66b096ad80a2da8863557d4b540291d3c721ddbd
xamry <amry.ks@gmail.com>
2012-08-23 19:00:32 +0530

    corrected entity definition

--- kundera-cassandra/src/test/java/com/impetus/client/persistence/CassandraEntitySample.java
+++ kundera-cassandra/src/test/java/com/impetus/client/persistence/CassandraEntitySample.java
@@ -28,7 +28,7 @@ import javax.persistence.Table;
  */
 
 @Entity
-@Table(name = "users", schema = "cassandra@KunderaExamples")
+@Table(name = "users", schema = "KunderaExamples@cassandra")
 // @NamedQuery(name="delete.query",query="Delete From CassandraEntitySample c where c.state=UP")
 public class CassandraEntitySample
 {
