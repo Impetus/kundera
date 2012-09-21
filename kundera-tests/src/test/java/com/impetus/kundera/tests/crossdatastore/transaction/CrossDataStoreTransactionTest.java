d1f61b0572eb2ffa996d10399d631eb76e8a81ca
kuld.cs.mishra@gmail.com <kuld.cs.mishra@gmail.com>
2012-09-22 01:05:37 +0530

    modified for embedded

--- kundera-tests/src/test/java/com/impetus/kundera/tests/crossdatastore/transaction/CrossDataStoreTransactionTest.java
+++ kundera-tests/src/test/java/com/impetus/kundera/tests/crossdatastore/transaction/CrossDataStoreTransactionTest.java
@@ -49,8 +49,8 @@ public class CrossDataStoreTransactionTest
         emf = Persistence.createEntityManagerFactory("secIdxAddCassandra,addMongo");
         em = emf.createEntityManager();
 
-        // CassandraCli.cassandraSetUp();
-        // CassandraCli.createKeySpace("KunderaTests");
+        CassandraCli.cassandraSetUp();
+        CassandraCli.createKeySpace("KunderaTests");
     }
 
     @Test
@@ -80,7 +80,7 @@ public class CrossDataStoreTransactionTest
     @After
     public void tearDown() throws Exception
     {
-        // CassandraCli.dropKeySpace("KunderaExamples");
+        CassandraCli.dropKeySpace("KunderaExamples");
     }
 
 }
