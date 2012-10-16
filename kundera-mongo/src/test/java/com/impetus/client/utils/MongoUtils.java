b74de2b77ed842a6c85279d78da96515e2af2fdf
kuld.cs.mishra@gmail.com <kuld.cs.mishra@gmail.com>
2012-10-16 18:56:53 +0530

    Added utility class for dropdatabase in mongoDb test

---
+++ kundera-mongo/src/test/java/com/impetus/client/utils/MongoUtils.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * * Copyright 2012 Impetus Infotech.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ ******************************************************************************/
+package com.impetus.client.utils;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.impetus.client.mongodb.MongoDBClient;
+import com.impetus.kundera.client.Client;
+import com.mongodb.DB;
+
+/**
+ * @author Kuldeep Mishra
+ * 
+ */
+public class MongoUtils
+{
+
+    private static final Log logger = LogFactory.getLog(MongoUtils.class);
+
+    /**
+     * 
+     */
+    public static void dropDatabase(EntityManagerFactory emf, String pu)
+    {
+        EntityManager em = emf.createEntityManager();
+
+        Map<String, Client> clients = (Map<String, Client>) em.getDelegate();
+        MongoDBClient client = (MongoDBClient) clients.get(pu);
+        if (client != null)
+        {
+            try
+            {
+                Field db = client.getClass().getDeclaredField("mongoDb");
+                if (!db.isAccessible())
+                {
+                    db.setAccessible(true);
+                }
+                DB mongoDB = (DB) db.get(client);
+                mongoDB.dropDatabase();
+            }
+            catch (SecurityException e)
+            {
+                logger.error(e);
+            }
+
+            catch (NoSuchFieldException e)
+            {
+                logger.error(e);
+            }
+            catch (IllegalArgumentException e)
+            {
+                logger.error(e);
+            }
+            catch (IllegalAccessException e)
+            {
+                logger.error(e);
+            }
+        }
+
+    }
+}
