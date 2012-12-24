cfb0d27b357666dc1f1f0ac6ff5f3de8ee79424c
xamry <amresh1002@impetus.co.in>
2012-12-24 13:24:18 +0530

    added skeleton code for Neo4J implementation

---
+++ kundera-neo4j/src/main/java/com/impetus/client/neo4j/Neo4JClient.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright 2012 Impetus Infotech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.impetus.client.neo4j;
+
+import java.util.List;
+import java.util.Map;
+
+import com.impetus.kundera.client.Client;
+import com.impetus.kundera.db.RelationHolder;
+import com.impetus.kundera.metadata.model.EntityMetadata;
+import com.impetus.kundera.persistence.EntityReader;
+import com.impetus.kundera.persistence.context.jointable.JoinTableData;
+import com.impetus.kundera.query.LuceneQuery;
+
+/**
+ * Implementation of {@link Client} using Neo4J Native Java driver 
+ * (see Embedded Java driver at http://www.neo4j.org/develop/drivers)  
+ * @author amresh.singh
+ */
+public class Neo4JClient extends Neo4JClientBase implements Client<LuceneQuery>
+{
+
+    @Override
+    public void populateClientProperties(Client client, Map<String, Object> properties)
+    {
+    }
+
+    @Override
+    public Object find(Class entityClass, Object key)
+    {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> findAll(Class<E> entityClass, Object... keys)
+    {
+        return null;
+    }
+
+    @Override
+    public <E> List<E> find(Class<E> entityClass, Map<String, String> embeddedColumnMap)
+    {
+        return null;
+    }
+
+    @Override
+    public void close()
+    {
+    }
+
+    @Override
+    public void delete(Object entity, Object pKey)
+    {
+    }
+
+    @Override
+    public void persistJoinTable(JoinTableData joinTableData)
+    {
+    }
+
+    @Override
+    public <E> List<E> getColumnsById(String schemaName, String tableName, String pKeyColumnName, String columnName,
+            Object pKeyColumnValue)
+    {
+        return null;
+    }
+
+    @Override
+    public Object[] findIdsByColumn(String schemaName, String tableName, String pKeyName, String columnName,
+            Object columnValue, Class entityClazz)
+    {
+        return null;
+    }
+
+    @Override
+    public void deleteByColumn(String schemaName, String tableName, String columnName, Object columnValue)
+    {
+    }
+
+    @Override
+    public List<Object> findByRelation(String colName, Object colValue, Class entityClazz)
+    {
+        return null;
+    }
+
+    @Override
+    public EntityReader getReader()
+    {
+        return null;
+    }
+
+    @Override
+    public Class<LuceneQuery> getQueryImplementor()
+    {
+        return null;
+    }
+
+    @Override
+    protected void onPersist(EntityMetadata entityMetadata, Object entity, Object id, List<RelationHolder> rlHolders)
+    {
+    } 
+    
+
+}
---
+++ kundera-neo4j/src/main/java/com/impetus/client/neo4j/Neo4JEntityReader.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2012 Impetus Infotech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.impetus.client.neo4j;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.impetus.kundera.client.Client;
+import com.impetus.kundera.client.EnhanceEntity;
+import com.impetus.kundera.metadata.model.EntityMetadata;
+import com.impetus.kundera.persistence.AbstractEntityReader;
+import com.impetus.kundera.persistence.EntityReader;
+import com.impetus.kundera.persistence.PersistenceDelegator;
+
+/**
+ * Entity Reader for Neo4J 
+ * @author amresh.singh
+ */
+public class Neo4JEntityReader  extends AbstractEntityReader implements EntityReader
+{
+
+    
+    
+
+    @Override
+    public EnhanceEntity findById(Object primaryKey, EntityMetadata m, Client client)
+    {
+        return super.findById(primaryKey, m, client);
+    }
+
+    @Override
+    public List<EnhanceEntity> populateRelation(EntityMetadata m, Client client)
+    {
+        return null;
+    }
+
+    @Override
+    public Object recursivelyFindEntities(Object entity, Map<String, Object> relationsMap, EntityMetadata m,
+            PersistenceDelegator pd)
+    {
+        return super.recursivelyFindEntities(entity, relationsMap, m, pd);
+    }
+
+    @Override
+    protected List<EnhanceEntity> onAssociationUsingLucene(EntityMetadata m, Client client, List<EnhanceEntity> ls)
+    {
+        return super.onAssociationUsingLucene(m, client, ls);
+    }
+
+    @Override
+    protected List<EnhanceEntity> transform(EntityMetadata m, List<EnhanceEntity> ls, List resultList)
+    {
+        return super.transform(m, ls, resultList);
+    }
+
+    @Override
+    protected Set<String> fetchDataFromLucene(Client client)
+    {
+        return super.fetchDataFromLucene(client);
+    }
+
+    @Override
+    protected Object getId(Object entity, EntityMetadata metadata)
+    {
+        return super.getId(entity, metadata);
+    }
+
+   
+    
+
+}
