a801faa34fe93a41e0ede91e8ae4786e5fb674cb
kuld.cs.mishra@gmail.com <kuld.cs.mishra@gmail.com>
2013-01-23 11:53:26 +0530

    Added null check

--- kundera-core/src/main/java/com/impetus/kundera/loader/PersistenceXMLLoader.java
+++ kundera-core/src/main/java/com/impetus/kundera/loader/PersistenceXMLLoader.java
@@ -161,7 +161,10 @@ public class PersistenceXMLLoader
         {
             try
             {
-                is.close();
+                if (is != null)
+                {
+                    is.close();
+                }
             }
             catch (IOException e)
             {
