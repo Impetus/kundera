b74f83e9a5e6b6c79ef4e326e854bdfbb47b1fbe
Vivek Mishra <vivek.mishra@yahoo.com>
2013-03-01 18:34:35 +0530

    Fix for issue #189

---
+++ kundera-cassandra/src/test/java/com/impetus/client/crud/Token.java
@@ -0,0 +1,41 @@
+package com.impetus.client.crud;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tokens", schema = "myapp@myapp_pu")
+
+public class Token
+{
+    @Id
+    @Column(name = "token_id")
+    private String id;
+    @ManyToOne(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id")
+    private TokenClient client;
+    public String getId()
+    {
+        return id;
+    }
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+    public TokenClient getClient()
+    {
+        return client;
+    }
+    public void setClient(TokenClient client)
+    {
+        this.client = client;
+    }
+    
+    
+}
