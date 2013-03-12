2282d63b33a56c42d683e57d2f7002803e1b0862
xamry <amresh1002@impetus.co.in>
2013-03-12 11:20:12 +0530

    added test case for capped collection

---
+++ kundera-mongo/src/test/java/com/impetus/client/crud/CappedCollectionTest.java
@@ -0,0 +1,184 @@
+/**
+ * Copyright 2012 Impetus Infotech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.impetus.client.crud;
+
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.impetus.client.mongodb.MongoDBClient;
+import com.impetus.client.mongodb.MongoDBConstants;
+import com.impetus.client.mongodb.MongoEntityReader;
+import com.impetus.client.mongodb.schemamanager.MongoDBEntitySimple;
+import com.impetus.kundera.client.Client;
+import com.mongodb.CommandResult;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+
+/**
+ * Test case for Capped Collections 
+ * @author amresh.singh
+ */
+public class CappedCollectionTest
+{
+    private DB db;
+
+    private String persistenceUnit = "mongoSchemaGenerationTest";
+
+    private EntityManagerFactory emf;
+
+    private EntityManager em;
+
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+        emf = Persistence.createEntityManagerFactory(persistenceUnit);
+        em = emf.createEntityManager();    
+        em.getDelegate();
+        getDB();
+     }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+        truncateMongo();
+    }
+    
+    @Test
+    public void testCappedCollection()
+    {
+        //Validate schema
+        DBCollection collection = db.getCollection("MongoDBEntitySimple");
+        Assert.assertTrue(collection.isCapped());
+        CommandResult stats = collection.getStats();       
+        
+        Object maxObj = stats.get(MongoDBConstants.MAX);
+        Assert.assertNotNull(maxObj);        
+        int max = Integer.parseInt(maxObj.toString());
+        Assert.assertEquals(10, max);
+        
+      /*  Object size = stats.get(MongoDBConstants.SIZE);
+        Assert.assertNotNull(size);
+        Assert.assertEquals(10240, Integer.parseInt(size.toString()));*/
+        
+        //Index should not have been created
+        List<DBObject> indexInfo = collection.getIndexInfo();
+        Assert.assertTrue(indexInfo.isEmpty());
+        
+        //Insert "Max" records successfully
+        for(int i = 1; i <= max; i++)
+        {
+            MongoDBEntitySimple entity = new MongoDBEntitySimple();
+            entity.setPersonId(i + "");
+            entity.setPersonName("Person_Name_" + i);
+            entity.setAge((short)i);
+            
+            em.persist(entity);            
+        }
+        
+        //Insert one more record and check whether it replaces first one
+        MongoDBEntitySimple entity = new MongoDBEntitySimple();
+        entity.setPersonId((max + 1) + "");
+        entity.setPersonName("Person_Name_" + (max + 1));
+        entity.setAge((short) (max + 1));        
+        em.persist(entity);
+        
+        em.clear();
+        MongoDBEntitySimple firstRecord = em.find(MongoDBEntitySimple.class, "1");
+        Assert.assertNull(firstRecord);
+        
+        MongoDBEntitySimple lastRecord = em.find(MongoDBEntitySimple.class, "" + (max + 1));
+        Assert.assertNotNull(lastRecord);        
+       
+        //Deleting documents within a capped collection should simply ignore, as is done by native API
+        em.remove(lastRecord);
+        em.clear();
+        lastRecord = em.find(MongoDBEntitySimple.class, "" + (max + 1));
+        Assert.assertNotNull(lastRecord);     
+                
+    }
+    
+    
+    private void truncateMongo()
+    {
+
+        if (db != null)
+        {
+            db.dropDatabase();
+        }
+    }  
+    
+    /**
+     * 
+     */
+    private void getDB()
+    {
+        Map<String, Client> clients = (Map<String, Client>) em.getDelegate();
+        MongoDBClient client = (MongoDBClient) clients.get(persistenceUnit);
+        if (client != null)
+        {
+            try
+            {
+                Field mongodb = client.getClass().getDeclaredField("mongoDb");
+                if (!mongodb.isAccessible())
+                {
+                    mongodb.setAccessible(true);
+                }
+                db = (DB) mongodb.get(client);
+            }
+
+            catch (SecurityException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (NoSuchFieldException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (IllegalArgumentException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (IllegalAccessException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
