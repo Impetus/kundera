908799854433fe9decee5079c7f2553bad0ec770
kuld.cs.mishra@gmail.com <kuld.cs.mishra@gmail.com>
2013-05-09 11:20:44 +0530

    Fixed Class loading issue

--- kundera-core/src/main/java/com/impetus/kundera/classreading/ClasspathReader.java
+++ kundera-core/src/main/java/com/impetus/kundera/classreading/ClasspathReader.java
@@ -145,6 +145,8 @@ public class ClasspathReader extends Reader
     private URL[] findResourcesInUrls(String classRelativePath, URL[] urls)
     {
         List<URL> list = new ArrayList<URL>();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
         for (URL url : urls)
         {
             if (AllowedProtocol.isValidProtocol(url.getProtocol().toUpperCase()) && url.getPath().endsWith(".jar"))
@@ -165,14 +167,11 @@ public class ClasspathReader extends Reader
                             List<URL> subList = new ArrayList<URL>();
                             for (String cpEntry : classPath.split(" "))
                             {
-                                try
+                                URLClassLoader loader = (URLClassLoader) this.getClass().getClassLoader();
+                                URL enclosedJar = loader.findResource(cpEntry);
+                                if (enclosedJar != null)
                                 {
-                                    subList.add(new URL(cpEntry));
-                                }
-                                catch (MalformedURLException e)
-                                {
-                                    logger.warn("Incorrect URL in the classpath of a jar file [" + url.toString()
-                                            + "]: " + cpEntry);
+                                    subList.add(enclosedJar);
                                 }
                             }
                             list.addAll(Arrays.asList(findResourcesInUrls(classRelativePath,
