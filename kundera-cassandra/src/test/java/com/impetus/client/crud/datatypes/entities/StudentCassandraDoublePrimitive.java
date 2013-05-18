f805f02c0f4ae70bb09a2280df194b038076aed2
Paulo Pires <pjpires@gmail.com>
2013-05-18 16:07:37 +0100

    Refactored tests with case-sensitive names. Fixes #268
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

---
+++ kundera-cassandra/src/test/java/com/impetus/client/crud/datatypes/entities/StudentCassandraDoublePrimitive.java
@@ -0,0 +1,77 @@
+package com.impetus.client.crud.datatypes.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.impetus.kundera.index.Index;
+import com.impetus.kundera.index.IndexCollection;
+
+@Entity
+@Table(name = "StudentCassandraDoublePrimitive", schema = "KunderaCassandraDataType@CassandraDataTypeTest")
+@IndexCollection(columns = { @Index(name = "age"), @Index(name = "name") })
+public class StudentCassandraDoublePrimitive
+{
+
+    @Id
+    private double id;
+
+    @Column(name = "AGE")
+    private short age;
+
+    @Column(name = "NAME")
+    private String name;
+
+    /**
+     * @return the id
+     */
+    public double getId()
+    {
+        return id;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(double id)
+    {
+        this.id = id;
+    }
+
+    /**
+     * @return the age
+     */
+    public short getAge()
+    {
+        return age;
+    }
+
+    /**
+     * @param age
+     *            the age to set
+     */
+    public void setAge(short age)
+    {
+        this.age = age;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
---
+++ kundera-mongo/src/test/java/com/impetus/client/crud/datatypes/entities/StudentMongoFloatPrimitive.java
@@ -0,0 +1,72 @@
+package com.impetus.client.crud.datatypes.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "StudentMongoFloatPrimitive", schema = "KunderaMongoDataType@MongoDataTypeTest")
+public class StudentMongoFloatPrimitive
+{
+    @Id
+    private float id;
+
+    @Column(name = "AGE")
+    private short age;
+
+    @Column(name = "NAME")
+    private String name;
+
+    /**
+     * @return the id
+     */
+    public float getId()
+    {
+        return id;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(float id)
+    {
+        this.id = id;
+    }
+
+    /**
+     * @return the age
+     */
+    public short getAge()
+    {
+        return age;
+    }
+
+    /**
+     * @param age
+     *            the age to set
+     */
+    public void setAge(short age)
+    {
+        this.age = age;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
