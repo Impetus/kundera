690851094aa55a2c287726bed2d144ea4fbcf89a
Vivek Mishra <vivek.mishra@yahoo.com>
2013-08-02 20:10:24 +0530

    junit coverage

--- kundera-core/src/main/java/com/impetus/kundera/service/policy/RetryService.java
+++ kundera-core/src/main/java/com/impetus/kundera/service/policy/RetryService.java
@@ -1,19 +1,29 @@
+/*******************************************************************************
+ * * Copyright 2013 Impetus Infotech.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ ******************************************************************************/
 package com.impetus.kundera.service.policy;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.impetus.kundera.service.Host;
 import com.impetus.kundera.utils.KunderaThreadFactory;
 
 public abstract class RetryService
 {
-    /** log for this class. */
-    private static Log logger = LogFactory.getLog(RetryService.class);
 
     protected final ScheduledExecutorService executor;
 
