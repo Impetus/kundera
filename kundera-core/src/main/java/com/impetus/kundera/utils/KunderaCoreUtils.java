80dc63b86f0bd98682f67d1d258d6641ecff1eb3
kuld.cs.mishra@gmail.com <kuld.cs.mishra@gmail.com>
2013-08-22 11:31:41 +0530

    Fixes for issue #360

--- kundera-core/src/main/java/com/impetus/kundera/utils/KunderaCoreUtils.java
+++ kundera-core/src/main/java/com/impetus/kundera/utils/KunderaCoreUtils.java
@@ -1,7 +1,10 @@
 package com.impetus.kundera.utils;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.impetus.kundera.property.PropertyAccessorHelper;
 
 public class KunderaCoreUtils
 {
@@ -47,5 +50,30 @@ public class KunderaCoreUtils
             throw new InvalidConfigurationException(
                     "For cross data store persistence, please specify as: Map {pu,Map of properties}");
         }
+    }
+
+    public static boolean isEmptyOrNull(Object o)
+    {
+        if (o == null)
+        {
+            return true;
+        }
+        if (PropertyAccessorHelper.isCollection(o.getClass()))
+        {
+            Collection c = (Collection) o;
+            if (c.isEmpty())
+            {
+                return true;
+            }
+        }
+        else if (Map.class.isAssignableFrom(o.getClass()))
+        {
+            Map m = (Map) o;
+            if (m.isEmpty())
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
