4081738c1204590895e826ba89043d7d37b39331
Chhavi Gangwal <chhavigangwal@gmail.com>
2013-10-09 14:40:40 +0530

    315

---
+++ src/kundera-tests/src/test/java/com/impetus/kundera/tests/entities/UserCassandraNoAnnotation.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * * Copyright 2012 Impetus Infotech.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ ******************************************************************************/
+package com.impetus.kundera.tests.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+
+import com.impetus.kundera.index.Index;
+import com.impetus.kundera.index.IndexCollection;
+
+@Entity
+@IndexCollection(columns = { @Index(name = "personName") })
+public class UserCassandraNoAnnotation
+{
+    @Id
+    @Column(name = "PERSON_ID")
+    private int personId;
+
+    @Column(name = "PERSON_NAME")
+    private String personName;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ADDRESS_ID")
+    private AddressMongoNoAnnotation address;
+
+    public int getPersonId()
+    {
+        return personId;
+    }
+    
+    public void setPersonId(int personId)
+    {
+        this.personId = personId;
+    }
+
+
+    public String getPersonName()
+    {
+        return personName;
+    }
+
+    public void setPersonName(String personName)
+    {
+        this.personName = personName;
+    }
+
+    public AddressMongoNoAnnotation getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(AddressMongoNoAnnotation address)
+    {
+        this.address = address;
+    }
+
+}
