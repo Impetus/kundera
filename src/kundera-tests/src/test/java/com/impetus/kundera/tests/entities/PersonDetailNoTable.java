71689d83f1c2d9814525f2d57e41af22fdd1d8bc
Chhavi Gangwal <chhavigangwal@gmail.com>
2013-10-17 17:01:36 +0530

    413

---
+++ src/kundera-tests/src/test/java/com/impetus/kundera/tests/entities/PersonDetailNoTable.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * * Copyright 2013 Impetus Infotech.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ ******************************************************************************/
+package com.impetus.kundera.tests.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+/**
+ * The Class PersonnelDTO.
+ * 
+ * @author Chhavi Gangwal
+ */
+
+@Entity
+public class PersonDetailNoTable
+{
+    
+    /** The person id. */
+    @Id
+    private String personId;
+
+    /** The first name. */
+    @Column(name = "first_name")
+    private String firstName;
+
+    /** The last name. */
+    @Column(name = "last_name")
+    private String lastName;
+
+    /**
+     * Instantiates a new personnel dto.
+     * 
+     * @param personId
+     *            the person id
+     * @param firstName
+     *            the first name
+     * @param lastName
+     *            the last name
+     */
+    public PersonDetailNoTable(String personId, String firstName, String lastName)
+    {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * Instantiates a new personnel dto.
+     */
+    public PersonDetailNoTable()
+    {
+
+    }
+
+    /**
+     * Gets the person id.
+     * 
+     * @return the personId
+     */
+    public String getPersonId()
+    {
+        return personId;
+    }
+
+    /**
+     * Sets the person id.
+     * 
+     * @param personId
+     *            the personId to set
+     */
+    public void setPersonId(String personId)
+    {
+        this.personId = personId;
+    }
+
+    /**
+     * Gets the first name.
+     * 
+     * @return the firstName
+     */
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    /**
+     * Sets the first name.
+     * 
+     * @param firstName
+     *            the firstName to set
+     */
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets the last name.
+     * 
+     * @return the lastName
+     */
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    /**
+     * Sets the last name.
+     * 
+     * @param lastName
+     *            the lastName to set
+     */
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+
+}
---
+++ src/kundera-tests/src/test/java/com/impetus/kundera/tests/entities/PersonRDBMSPolyglot.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * * Copyright 2013 Impetus Infotech.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ ******************************************************************************/
+package com.impetus.kundera.tests.entities;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Person.
+ */
+@Entity
+public class PersonRDBMSPolyglot
+{
+
+    /** The person id. */
+    @Id
+    @Column(name = "PERSON_ID")
+    private String personId;
+
+    /** The person name. */
+    @Column(name = "PERSON_NAME")
+    private String personName;
+
+    /** The age. */
+    @Column(name = "AGE")
+    private Integer age;
+    
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ADDRESS_ID")
+    private AddressCassandra address;
+
+    /**
+     * Gets the person id.
+     * 
+     * @return the person id
+     */
+    public String getPersonId()
+    {
+        return personId;
+    }
+
+    /**
+     * Gets the person name.
+     * 
+     * @return the person name
+     */
+    public String getPersonName()
+    {
+        return personName;
+    }
+
+    /**
+     * Sets the person name.
+     * 
+     * @param personName
+     *            the new person name
+     */
+    public void setPersonName(String personName)
+    {
+        this.personName = personName;
+    }
+
+    /**
+     * Sets the person id.
+     * 
+     * @param personId
+     *            the new person id
+     */
+    public void setPersonId(String personId)
+    {
+        this.personId = personId;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge()
+    {
+        return age;
+    }
+
+    /**
+     * @param age
+     *            the age to set
+     */
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+    
+    /**
+     * @param address
+     *            the address to set
+     */
+    public void setAddress(AddressCassandra address)
+    {
+        this.address = address;
+    }
+    
+    /**
+     * @return the address
+     */
+    public AddressCassandra getAddress()
+    {
+        return address;
+    }
+
+    
+
+}
