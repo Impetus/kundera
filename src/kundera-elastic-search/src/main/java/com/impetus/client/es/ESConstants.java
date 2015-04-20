78f43448248d50cad4cf8abef7ef20effee72fe3
Amit Kumar <amitr.kumar@impetus.co.in>
2015-04-20 19:59:33 +0000

    #708

---
+++ src/kundera-elastic-search/src/main/java/com/impetus/client/es/ESConstants.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *  * Copyright 2015 Impetus Infotech.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ ******************************************************************************/
+package com.impetus.client.es;
+
+/**
+ * Constants for ES.
+ * 
+ * @author Amit Kumar
+ */
+public interface ESConstants
+{
+    /** The Constant dot. */
+    public static final char dot = '.';
+
+    /** The Constant leftBracket. */
+    public static final char leftBracket = '(';
+
+    /** The Constant rightBracket. */
+    public static final char rightBracket = ')';
+
+    /** The Constant asterisk. */
+    public static final String asterisk = "*";
+
+    /** The Constant percentage. */
+    public static final String percentage = "%";
+
+    /** The Constant infinity. */
+    public static final String infinity = "INFINITY";
+
+    /** The Constant aggName. */
+    public static final String aggName = "esAggs";
+}
