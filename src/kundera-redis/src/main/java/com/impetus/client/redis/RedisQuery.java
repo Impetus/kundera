06752c71f3a5e5352900380510f07ac1f0f2ce7b
Amit Kumar <amitr.kumar@impetus.co.in>
2015-05-08 17:11:02 +0000

    #739

--- src/kundera-redis/src/main/java/com/impetus/client/redis/RedisQuery.java
+++ src/kundera-redis/src/main/java/com/impetus/client/redis/RedisQuery.java
@@ -16,8 +16,8 @@
 package com.impetus.client.redis;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
@@ -78,7 +78,7 @@ public class RedisQuery extends QueryImpl
          * 
          */
 
-        Set<Object> results = new HashSet<Object>();
+        Set<Object> results = new LinkedHashSet<Object>();
 
         ClientMetadata clientMetadata = ((ClientBase) client).getClientMetadata();
 
